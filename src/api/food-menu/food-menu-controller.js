65235a7a3a277a714b607ae84f8c6d55d2fc66f4
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-13 19:28:44 +0200

    implement getMenuItems and getMenuItemById in food-menu-controller.js

---
+++ src/api/food-menu/food-menu-controller.js
@@ -0,0 +1,52 @@
+import {
+  listAllMenuItems,
+  findMenuItemById,
+  addMenuItem,
+  modifyMenuItem,
+  removeMenuItem,
+} from './food-menu-model.js';
+
+const getMenuItems = async (req, res) => {
+  res.json(await listAllMenuItems());
+};
+
+const getMenuItemById = async (req, res) => {
+  const item = await findMenuItemById(req.params.id);
+
+  if (item) {
+    res.status(201).json(item);
+  } else {
+    res.status(404).json({message: "Could not find item"});
+  }
+};
+
+/*const getCatById = (req, res) => {
+  const cat = findCatById(req.params.id);
+  if (cat) {
+    res.json(cat);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+const postCat = (req, res) => {
+  const result = addCat(req.body);
+  if (result.cat_id) {
+    res.status(201);
+    res.json({message: 'New cat added.', result});
+  } else {
+    res.sendStatus(400);
+  }
+};
+
+const putCat = (req, res) => {
+  // not implemented in this example, this is homework
+  res.sendStatus(200);
+};
+
+const deleteCat = (req, res) => {
+  // not implemented in this example, this is homework
+  res.sendStatus(200);
+};*/
+
+export {getMenuItems, getMenuItemById};
