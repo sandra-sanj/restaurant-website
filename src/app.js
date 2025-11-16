ff2e8762ff1bc490472c6ff640b1d6b8c6ef4fd8
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-16 22:45:37 +0200

    add uploads and error handlers to app.js for future implementation

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import api from './api/index.js';
+import {errorHandler, notFoundHandler} from './middlewares/error-handlers.js';
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('public')); // website is served from public folder
+app.use('/uploads', express.static('uploads'));
+
 app.use(express.json()); // parse json data from http request
 app.use(express.urlencoded({extended: true}));
 app.use('/api/v1', api); // adds prefix and guides all requests to routes inside api
+
+app.use(notFoundHandler); // default for all routes not handled by routers above
+app.use(errorHandler); // error handler
 
 export default app;
