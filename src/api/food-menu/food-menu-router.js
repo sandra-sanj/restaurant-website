6fdb07beef8320d75f8b5d1d916507d87a406f09
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-16 22:46:16 +0200

    add putMenuItem and deleteMenuItem routes to food-menu-router.js

--- src/api/food-menu/food-menu-router.js
+++ src/api/food-menu/food-menu-router.js
@@ -3,12 +3,20 @@ import {
   getMenuItems,
   getMenuItemById,
   postMenuItem,
+  putMenuItem,
+  deleteMenuItem,
 } from './food-menu-controller.js';
 
 const foodMenuRouter = express.Router();
 
+// TODO: modify post, put and delete to require authentication from user with admin role
+
 foodMenuRouter.route('/').get(getMenuItems).post(postMenuItem);
 
-foodMenuRouter.route('/:id').get(getMenuItemById); //.put(putCat).delete(deleteCat);
+foodMenuRouter
+  .route('/:id')
+  .get(getMenuItemById)
+  .put(putMenuItem)
+  .delete(deleteMenuItem);
 
 export default foodMenuRouter;
