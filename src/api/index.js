ea97d95da60b54778e6bc021ee6ec5245a3b5f8c
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-18 08:57:55 +0200

    add /users and /auth to paths in index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import foodMenuRouter from './food-menu/food-menu-router.js';
+import userRouter from './user/user-router.js';
+import authRouter from './auth/auth-router.js';
 
 const router = express.Router();
 
 // bind base url to routers
 router.use('/menu', foodMenuRouter);
-//router.use("/user", userRouter);
+router.use('/users', userRouter);
+router.use('/auth', authRouter);
 
 export default router;
