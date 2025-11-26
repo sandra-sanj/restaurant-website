97a39b6b2166fb102fdc81b3d30e39eca6f5fc2b
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-26 16:07:43 +0200

    comment out console logs in authentication.js

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -2,11 +2,11 @@ import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 
 const authenticateToken = (req, res, next) => {
-  console.log('authenticateToken', req.headers);
+  //console.log('authenticateToken', req.headers);
 
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-  console.log('token', token);
+  //console.log('token', token);
 
   if (token == null) {
     return res.sendStatus(401);
