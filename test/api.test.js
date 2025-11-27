cd47beec3c65deea1a8263ab9c04a633b98e6a5a
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-11-27 10:08:34 +0200

    move variables outside functions, move auth tests outside user tests, add test to test failed user login; api.test.js

--- test/api.test.js
+++ test/api.test.js
@@ -2,26 +2,37 @@ import request from 'supertest';
 import app from '../src/app.js'; // your Express app
 import {closePool} from '../src/utils/database.js';
 
+let userId;
+let token;
+
+let username;
+let password;
+let email;
+let phone;
+let role;
+
+beforeAll(async () => {
+  // user to create during tests
+  username = 'Sienna2';
+  password = 'password123';
+  email = 'sienna2@example.fi';
+  phone = '+358998877665';
+  role = 'customer';
+});
+
 afterAll(async () => {
   await closePool();
 });
 
 describe('Test user endpoints', () => {
-  let userId;
-  let token;
-
-  const username = 'Sienna2';
-  const password = 'password123';
-  const email = 'sienna2@example.fi';
-
   describe('POST /api/v1/users', () => {
     it('should create a new user', async () => {
       const newUser = {
         username: username,
         email: email,
         password: password,
-        phone: '+358998877665',
-        role: 'customer',
+        phone: phone,
+        role: role,
         is_active: 1,
       };
       const res = await request(app)
@@ -44,8 +55,8 @@ describe('Test user endpoints', () => {
         username: username,
         email: email,
         password: password,
-        phone: '+358998877665',
-        role: 'customer',
+        phone: phone,
+        role: role,
         is_active: 1,
       };
       const res = await request(app)
@@ -57,47 +68,64 @@ describe('Test user endpoints', () => {
       expect(res.statusCode).toEqual(409);
     });
   });
+});
 
-  describe('Test Authentication endpoints', () => {
-    describe('POST /api/v1/auth/login', () => {
-      it('should login a user and return a token', async () => {
-        const user = {
-          username: username,
-          password: password,
-        };
-        const res = await request(app)
-          .post('/api/v1/auth/login')
-          .send(user)
-          .set('Accept', 'application/json');
-        // TODO: add all relevant assertions here
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty('user');
-        expect(res.body.token).toBeDefined();
-        token = res.body.token;
-      });
-    });
-
-    describe('GET /api/v1/auth/me', () => {
-      it('should return a list of users', async () => {
-        const res = await request(app)
-          .get('/api/v1/auth/me')
-          .set('Authorization', `Bearer ${token}`)
-          .set('Accept', 'application/json');
-
-        // save user id if user creation failed
-        if (typeof userId === 'undefined') {
-          userId = res.body.user.user_id;
-        }
-
-        // TODO: add all relevant assertions here
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty('user');
-      });
+describe('Test Authentication endpoints', () => {
+  describe('POST /api/v1/auth/login', () => {
+    it('should fail login a user and not return a token', async () => {
+      const user = {
+        username: username + '1',
+        password: password,
+      };
+      const res = await request(app)
+        .post('/api/v1/auth/login')
+        .send(user)
+        .set('Accept', 'application/json');
+      // TODO: add all relevant assertions here
+      expect(res.statusCode).toEqual(401);
     });
   });
 
-  // TODO: test user modification (POST)
+  describe('POST /api/v1/auth/login', () => {
+    it('should login a user and return a token', async () => {
+      const user = {
+        username: username,
+        password: password,
+      };
+      const res = await request(app)
+        .post('/api/v1/auth/login')
+        .send(user)
+        .set('Accept', 'application/json');
+      // TODO: add all relevant assertions here
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty('user');
+      expect(res.body.token).toBeDefined();
+      token = res.body.token;
+    });
+  });
 
+  describe('GET /api/v1/auth/me', () => {
+    it('should get user info by valid token', async () => {
+      const res = await request(app)
+        .get('/api/v1/auth/me')
+        .set('Authorization', `Bearer ${token}`)
+        .set('Accept', 'application/json');
+
+      // save user id if user creation failed (user already exists)
+      if (typeof userId === 'undefined') {
+        userId = res.body.user.user_id;
+      }
+
+      // TODO: add all relevant assertions here
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty('user');
+    });
+  });
+});
+
+describe('Continue to test user endpoints', () => {
+  // TODO: test user modification (POST)
+  // TODO: test failed user deletion (no token)
   describe('DELETE /api/v1/users/:id', () => {
     it('should delete user', async () => {
       const res = await request(app)
