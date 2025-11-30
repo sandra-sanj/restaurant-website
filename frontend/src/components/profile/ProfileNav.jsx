dcb220706034ce86de6746c00a78115e141e934e
Anni <anni.alanen@metropolia.fi>
2025-11-30 19:31:11 +0200

    home page runko and profile page fix

--- frontend/src/components/profile/ProfileNav.jsx
+++
@@ -1,5 +0,0 @@
-import {Link} from "react-router";
-
-function MenuNav() {
-
-}
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -16,7 +16,18 @@ const Profile = () => {
     return (
         <div>
                 <>
-                    <h1>Burrito jäsen</h1> 
+                    <h1>Moi jäsen</h1> 
+                    <div className="profile-bar">
+                        <button onClick={() => 
+                            setShowModal('history')}>Historia
+                        </button>
+                        <button onClick={() =>
+                            setShowModal('payment')}>Maksu
+                        </button>
+                        <button onClick={() => 
+                            setShowModal('kieli')}>Kieli
+                        </button>
+                    </div>
                     <div className="edit-prof" onClick={() => setShowModal('name')}>
                         <h3>Nimi: nimi</h3>
                         <p>✎</p>
@@ -33,36 +44,26 @@ const Profile = () => {
                         <h3>Osoite: </h3>
                         <p>✎</p>
                     </div>     
-                    <div className="profile-bar">
-                        <button onClick={() => 
-                            setShowModal('history')}>Historia
-                        </button>
-                        <button onClick={() =>
-                            setShowModal('payment')}>Maksu
-                        </button>
-                        <button onClick={() => 
-                            setShowModal('kieli')}>Kieli
-                        </button>
 
-                        <Modal isOpen={showModal === 'name'} onClose={closeModal}>
-                            <label htmlFor="editName"> Nimi: 
-                                <input 
-                                type="text" 
-                                id="editName" 
-                                placeholder="  username"/>
-                            </label>    
-                        </Modal>
+                    <Modal isOpen={showModal === 'name'} onClose={closeModal}>
+                        <label htmlFor="editName"> Nimi: 
+                            <input 
+                            type="text" 
+                            id="editName" 
+                            placeholder="  username"/>
+                        </label>    
+                    </Modal>
 
-                        <Modal isOpen={showModal === 'history' } onClose={closeModal}>
-                            <h2>ei vielä historiaa</h2>
-                        </Modal>
-                        <Modal isOpen={showModal === 'payment' } onClose={closeModal}>
-                            <h2>Maksu metodit</h2>
-                        </Modal>
-                        <Modal isOpen={showModal === 'kieli' } onClose={closeModal}>
-                            <h2>kielesi on suomi</h2>
-                        </Modal>
-                    </div>
+                    <Modal isOpen={showModal === 'history' } onClose={closeModal}>
+                        <h2>ei vielä historiaa</h2>
+                    </Modal>
+                    <Modal isOpen={showModal === 'payment' } onClose={closeModal}>
+                        <h2>Maksu metodit</h2>
+                    </Modal>
+                    <Modal isOpen={showModal === 'kieli' } onClose={closeModal}>
+                        <h2>kielesi on suomi</h2>
+                    </Modal>
+
                     <button>Kirjaudu ulos</button>
                     <br/>
                     <button>Poista käyttäjä</button>
