4a0137cec292530b4c9d75e7ea9f547a322b8bee
Anni <anni.alanen@metropolia.fi>
2025-12-05 21:32:09 +0200

    should work

--- frontend/src/utils/fetchData.js
+++ frontend/src/utils/fetchData.js
@@ -1,14 +1,14 @@
 const fetchData = async (url, options = {}) => {
     const res = await fetch(url, options);
+    const response = await res.json();
     
     if(!res.ok) {
         const error = new Error('error');
-        //error.status = res.status;
+        error.status = res.status;
         throw error;
     }
-    //const response = await res.json();
 
-    return res;
+    return response;
 };
 
 export {fetchData};
