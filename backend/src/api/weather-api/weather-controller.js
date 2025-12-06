3946f66c40f12fc96b7cb7aef6f85f13ac3ccaeb
nurhana <nur.hana@metropolia.fi>
2025-12-06 12:09:04 +0200

    Add restaurant location example data for weather api

---
+++ backend/src/api/weather-api/weather-controller.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+
+// Restaurants location
+const RESTAURANT_LOCATION = {
+  name: 'Mexican Restaurant',
+  address: 'Myllypurontie 1, 00920 Helsinki',
+  city: 'Helsinki',
+  lat: 60.2094,
+  lon: 25.0778,
+};
