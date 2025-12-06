21e928d1c282aa044d8a5429060c514ec186ac21
Ira <ira.kumpula@metropolia.fi>
2025-12-06 13:48:57 +0200

    Update restaurant name and contacts

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,99 +1,103 @@
-import { useNavigate } from "react-router";
-import Login from "./Login";
-
+import {useNavigate} from 'react-router';
+import Login from './Login';
 
 //TODO: varmista että navigaatio oikeaan osoitteeseen
-// sitten ehkä oma funktio 
+// sitten ehkä oma funktio
 function Home() {
   const navigate = useNavigate();
 
-  const imgSrc = "/images/";
+  const imgSrc = '/images/';
 
-    return (
-        <>
-          <div>
-            <h1>Ravintolan nimi</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum veritatis suscipit quos dolorum </p>
-          </div>
-          <div>
-            <button onClick={() => navigate('/login/login')}>Kirjaudu</button>
-            <button onClick={() => navigate('/login/register')}>Rekisteröidy</button>
-          </div>
-          <div className="ad-div">
-            <div>
-              <img
-                src={imgSrc + "/home/home1.jpg"}
-                alt=""
-                width="200" height='500'
-                onClick={() => navigate('/menu')}
-                />
-              <p>Lounasmenu</p>
-            </div>  
-            <div>
-              <img
-                src={imgSrc + "/home/home2.jpg"}
-                alt=""
-                width="200" height='500'
-                onClick={() => navigate('/menu')}
-                />
-              <p>Uutuus: katkaraputaco</p>  
-            </div>    
-            <div>
-              <img
-                src={imgSrc + "/home/home3.jpg"}
-                alt=""
-                width="200" height='500'
-                onClick={() => navigate('/menu')}
-                />
-              <p>Joku teksti</p>  
-            </div>
-          </div>
-          <h3>MENU</h3>
-          <div className="container-home">
-            <div>
-              <img
-              src={imgSrc + "/home/mains.jpg"}
-              alt=""
-              onClick={() => navigate('/menu/mains')}
-              />
-              <p>PÄÄRUOAT</p>
-            </div>
-            <div>
-              <img
-              src={imgSrc + "/home/snacks.jpg"}
-              alt=""
-              onClick={() => navigate('/menu/snacks')}
-              />
-              <p>SNACKS</p>
-            </div>
-            <div>
-              <img
-              src={imgSrc + "/home/desserts.jpg"}
-              alt=""
-              onClick={() => navigate('/menu/desserts')}
-              />
-              <p>JÄLKIRUOAT</p>
-            </div>
-            <div>
-              <img
-              src={imgSrc + "/home/drinks.jpg"}
-              alt=""
-              onClick={() => navigate('/menu/drinks')}
-              />
-              <p>JUOMAT</p>
-            </div>
-          </div>
-          <h3>YHTEYSTIEDOT</h3>
-          <div className="contacts" id="contacts">
-            <p>Aukiolo ajat: </p>
-            <p>ma-pe: 11-22</p>
-            <p>la: 12-22</p>
-            <p>su: suljettu</p>
-            <p>Osoite: jokuosoite 8 a</p>
-            <p>Sähköposti: ravintolannimi@gmail.com</p>
-            <p>Puhelinnumero: +358 7776669</p>
-          </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <h1>Taqueria 21</h1>
+        <p>Pieni pala Meksikoa keskellä Helsinkiä</p>
+      </div>
+      <div>
+        <button onClick={() => navigate('/login/login')}>Kirjaudu</button>
+        <button onClick={() => navigate('/login/register')}>
+          Rekisteröidy
+        </button>
+      </div>
+      <div className="ad-div">
+        <div>
+          <img
+            src={imgSrc + '/home/home1.jpg'}
+            alt=""
+            width="200"
+            height="500"
+            onClick={() => navigate('/menu')}
+          />
+          <p>Lounasmenu</p>
+        </div>
+        <div>
+          <img
+            src={imgSrc + '/home/home2.jpg'}
+            alt=""
+            width="200"
+            height="500"
+            onClick={() => navigate('/menu')}
+          />
+          <p>Uutuus: katkaraputaco</p>
+        </div>
+        <div>
+          <img
+            src={imgSrc + '/home/home3.jpg'}
+            alt=""
+            width="200"
+            height="500"
+            onClick={() => navigate('/menu')}
+          />
+          <p>Joku teksti</p>
+        </div>
+      </div>
+      <h3>MENU</h3>
+      <div className="container-home">
+        <div>
+          <img
+            src={imgSrc + '/home/mains.jpg'}
+            alt=""
+            onClick={() => navigate('/menu/mains')}
+          />
+          <p>PÄÄRUOAT</p>
+        </div>
+        <div>
+          <img
+            src={imgSrc + '/home/snacks.jpg'}
+            alt=""
+            onClick={() => navigate('/menu/snacks')}
+          />
+          <p>SNACKS</p>
+        </div>
+        <div>
+          <img
+            src={imgSrc + '/home/desserts.jpg'}
+            alt=""
+            onClick={() => navigate('/menu/desserts')}
+          />
+          <p>JÄLKIRUOAT</p>
+        </div>
+        <div>
+          <img
+            src={imgSrc + '/home/drinks.jpg'}
+            alt=""
+            onClick={() => navigate('/menu/drinks')}
+          />
+          <p>JUOMAT</p>
+        </div>
+      </div>
+      <h3>YHTEYSTIEDOT</h3>
+      <div className="contacts" id="contacts">
+        <p>Aukioloajat: </p>
+        <p>ma-pe: 11-22</p>
+        <p>la: 12-22</p>
+        <p>su: suljettu</p>
+        <p>Osoite: Kalevankatu 21, 00100 Helsinki</p>
+        <p>Sähköposti: info@taqueria21.fi</p>
+        <p>Puhelinnumero: +358 7776669</p>
+      </div>
+    </>
+  );
 }
 export default Home;
