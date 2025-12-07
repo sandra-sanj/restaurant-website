64776f0b57dc1dcd4c962ff51746566e124f94e0
Ira <ira.kumpula@metropolia.fi>
2025-12-07 15:44:59 +0200

    Modify test-add-item.js item values

--- backend/test/test-add-item.js
+++ backend/test/test-add-item.js
@@ -1,16 +1,20 @@
 import promisePool from '../src/utils/database.js';
 
+// Add test item to database:
+// in terminal, run: cd backend -> node <file-path>
+// esim. node .\test\test-add-item.js
+
 (async () => {
   try {
     const [result] = await promisePool.execute(
       `INSERT INTO menu_items
       (category_id, name, name_en, description, description_en, price, is_available)
       VALUES (?, ?, ?, ?, ?, ?, ?)`,
-      [1, 'Testi Taco', 'Test Taco EN', 'Kuvaus', 'Description EN', 5.9, 1]
+      [3, 'Joku testijälkkäri', 'Test dessert EN', 'Kuvaus jotain jeejee', 'Description EN here', 11.9, 1]
     );
-    console.log('Lisätty rivi ID:', result.insertId);
+    console.log('Added item ID:', result.insertId);
   } catch (err) {
-    console.error('Lisäys epäonnistui:', err);
+    console.error('Add item failed:', err);
   } finally {
     process.exit();
   }
