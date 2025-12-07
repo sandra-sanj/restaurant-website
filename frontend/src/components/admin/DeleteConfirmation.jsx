cc2ec236d7a7cde40003f4230c827ea6b4ffd9a8
Ira <ira.kumpula@metropolia.fi>
2025-12-07 17:14:29 +0200

    Close DeleteConfirmation component after deleting item

--- frontend/src/components/admin/DeleteConfirmation.jsx
+++ frontend/src/components/admin/DeleteConfirmation.jsx
@@ -1,32 +1,43 @@
-import { useMenu } from "../../hooks/apiHook";
+import {useMenu} from '../../hooks/apiHook';
 
 const DeleteConfirmation = ({onClose, selectedItem}) => {
-
   const {deleteMenuItem} = useMenu();
-  
+
   const handleDeleteItemClick = async () => {
     console.log('Delete from delete confirmation pressed');
 
     const token = localStorage.getItem('token');
-    console.log('token DeleteConfirmationista:', token);
+    //console.log('token DeleteConfirmationista:', token);
 
-    const delResponse = await deleteMenuItem(selectedItem, token);
-
-    console.log('delResponse:', delResponse);
-  }
+    try {
+      const delResponse = await deleteMenuItem(selectedItem, token);
+      console.log('delResponse:', delResponse);
+      alert(`${selectedItem.name} poistettu`);
+      onClose();
+    } catch (e) {
+      console.log(e);
+      alert(`${selectedItem.name} ei poistettu, jotain meni vikaan`);
+    }
+  };
 
   return (
     <>
       <div className="m-5 outline-2 outline-gray-400 rounded-md">
         <div className="flex justify-end p-3">
-          <span className="cursor-pointer font-bold text-lg hover:opacity-80" onClick={onClose}>
+          <span
+            className="cursor-pointer font-bold text-lg hover:opacity-80"
+            onClick={onClose}
+          >
             &times;
           </span>
         </div>
         <p className="p-3 pt-0">Poista {selectedItem.name}?</p>
-        <button onClick={handleDeleteItemClick}
-        className="bg-[#982A2A]! text-white px-2 py-1 rounded-xl hover:opacity-90"
-        >Poista</button>
+        <button
+          onClick={handleDeleteItemClick}
+          className="bg-[#982A2A]! text-white px-2 py-1 rounded-xl hover:opacity-90"
+        >
+          Poista
+        </button>
       </div>
     </>
   );
