7fb50e4bda5ef564fea2c5ad49bad659f3edc504
nurhana <nur.hana@metropolia.fi>
2025-12-07 10:57:10 +0200

    add loading and error message

--- frontend/src/components/WeatherWidget.jsx
+++ frontend/src/components/WeatherWidget.jsx
@@ -3,20 +3,40 @@ import {useWeather} from '../hooks/apiHook.js';
 function WeatherWidget() {
   const {weather, loading, error} = useWeather();
 
+  if (loading) {
+    return (
+      <div>
+        <p>Ladataan säätietoja...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p>{error}</p>
+      </div>
+    );
+  }
+
+  if (!weather) {
+    return null;
+  }
+
   const {city, weather: weatherData, lastUpdated} = weather;
   const iconUrl = `https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`;
 
   return (
     <div className="max-w-[400px] max-h-[300px] mx-auto mt-4 p-6 rounded-2xl shadow-md">
       <div className="text-center mb-2">
-        <h3 className="text-xl font-semibold">🌤️ Sää - {city}</h3>
+        <h3 className="text-lg font-semibold mb-3">Sää - {city}</h3>
       </div>
 
-      <div className="flex items-center justify-center gap-5 mb-2">
+      <div className="flex items-center justify-center gap-4 mb-2">
         <img
           src={iconUrl}
           alt={weatherData.description}
-          className="w-[90px] h-[90px]"
+          className="w-20 h-20"
         />
       </div>
 
