71b6f6107ad489e8a5f278d57dfbd3193b375aff
nurhana <nur.hana@metropolia.fi>
2025-12-07 14:29:32 +0200

    Didn't add or modify anything (auto-format affected file)

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,12 @@
-import { HashLink} from "react-router-hash-link"
+import {HashLink} from 'react-router-hash-link';
 
 function Footer() {
-
-    return(
-        <footer className="footer">
-            <HashLink smooth to='/#contacts' >Contacts</HashLink>
-        </footer>
-    )
+  return (
+    <footer className="footer">
+      <HashLink smooth to="/#contacts">
+        Contacts
+      </HashLink>
+    </footer>
+  );
 }
 export default Footer;
