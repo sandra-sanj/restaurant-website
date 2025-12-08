dfdcab63e89d4c1d462eca7056c4e153eed7c186
nurhana <nur.hana@metropolia.fi>
2025-12-08 15:23:59 +0200

    Add navigation strings and installed react-localization

---
+++ frontend/src/localization/translations.js
@@ -0,0 +1,20 @@
+import LocalizedStrings from 'react-localization';
+
+const strings = new LocalizedStrings({
+  fi: {
+    // Navigation
+    nav: {
+      home: 'Koti',
+      menu: 'Menu',
+      admin: 'Ylläpito',
+      history: 'Historia',
+      profile: 'Profiili',
+      login: 'Kirjaudu',
+      cart: 'Ostoskori',
+    },
+    // Home page
+    home: {},
+  },
+});
+
+export default strings;
