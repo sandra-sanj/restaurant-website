56839d5f021e5198cc5c6dfede142500ae7b2498
Ira <ira.kumpula@metropolia.fi>
2025-12-08 21:20:11 +0200

    Display admin and history pages only when user role is admin

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,51 +1,78 @@
-import {Link} from "react-router";
-import { useUserContext } from "../hooks/contextHook";
-import { useEffect } from "react";
+import {Link} from 'react-router';
+import {useUserContext} from '../hooks/contextHook';
+import {useEffect} from 'react';
+import {useState} from 'react';
 
 function NavBar() {
-    const { handleAutoLogin, user} = useUserContext();
+  const {handleAutoLogin, user} = useUserContext();
+  const [isAdmin, setIsAdmin] = useState(false);
 
-    useEffect(() => {
-        handleAutoLogin();
-    }, []);
+  useEffect(() => {
+    handleAutoLogin();
+  }, []);
 
-    return <nav className="navbar h-12">
-        <div className="navbar-links">
-            <ul>
-                <li>
-                  <Link to="/" className="nav-link">Home </Link>
-                </li>
-                <li>
-                  <Link to="/menu" className="nav-link">Menu </Link>
-                </li>
-                <li>
-                  <Link to="/admin" className="nav-link">Admin </Link>
-                </li>
-                <li>
-                  <Link to="/history" className="nav-link">History </Link>
-                </li>
+  useEffect(() => {
+    setIsAdmin(user?.role === 'admin');
+  }, [user]);
+  
 
-                {user ? (
-                  <>
-                    <li>
-                    <Link to="/profile" className="nav-link">Profile </Link>
-                    </li>
-                  </>
-                ) : (
-                    <>
-                      <li>
-                        <Link to="/login" className="nav-link">LogIn </Link>
+  return (
+    <nav className="navbar h-12">
+      <div className="navbar-links">
+        <ul>
+          <li>
+            <Link to="/" className="nav-link">
+              Home{' '}
+            </Link>
+          </li>
+          <li>
+            <Link to="/menu" className="nav-link">
+              Menu{' '}
+            </Link>
+          </li>
 
-                      </li>
-                    </>
-                )}
-                
+          {user && isAdmin && (
+              <>
                 <li>
-                  <Link to="/cart" className="nav-link">Cart </Link>
+                  <Link to="/admin" className="nav-link">
+                    Admin{' '}
+                  </Link>
                 </li>
-            </ul>
-        </div>
+                <li>
+                  <Link to="/history" className="nav-link">
+                    History{' '}
+                  </Link>
+                </li>
+              </>,
+            )}
+
+          {user ? (
+            <>
+              <li>
+                <Link to="/profile" className="nav-link">
+                  Profile{' '}
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login" className="nav-link">
+                  LogIn{' '}
+                </Link>
+              </li>
+            </>
+          )}
+
+          <li>
+            <Link to="/cart" className="nav-link">
+              Cart{' '}
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
+  );
 }
 
 export default NavBar;
