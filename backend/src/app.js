8338f7334a6b35bcda5c26411440b0cc8d94c9d8
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-12-08 12:19:59 +0200

    attempt to fix server side routing in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,11 @@ import express from 'express';
 import api from './api/index.js';
 import {errorHandler, notFoundHandler} from './middlewares/error-handlers.js';
 import cors from 'cors';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -24,6 +29,11 @@ app.use('/uploads', express.static('uploads'));
 app.use(express.json()); // parse json data from http request
 app.use(express.urlencoded({extended: true}));
 app.use('/api/v1', api); // adds prefix and guides all requests to routes inside api
+
+// for server side routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use(notFoundHandler); // default for all routes not handled by routers above
 app.use(errorHandler); // error handler
