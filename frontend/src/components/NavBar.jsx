b28fa2f7078ee170abdfa5528f249119d5a45ae7
nurhana <nur.hana@metropolia.fi>
2025-12-08 20:55:11 +0200

    Add language toggle in NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,51 +1,75 @@
-import {Link} from "react-router";
-import { useUserContext } from "../hooks/contextHook";
-import { useEffect } from "react";
+import {Link} from 'react-router';
+import {useUserContext} from '../hooks/contextHook';
+import {useLanguage} from '../hooks/useLanguage';
+import {useEffect} from 'react';
 
 function NavBar() {
-    const { handleAutoLogin, user} = useUserContext();
+  const {handleAutoLogin, user} = useUserContext();
 
-    useEffect(() => {
-        handleAutoLogin();
-    }, []);
+  const {language, toggleLanguage, strings} = useLanguage();
 
-    return <nav className="navbar">
-        <div className="navbar-links">
-            <ul>
-                <li>
-                  <Link to="/" className="nav-link">Home </Link>
-                </li>
-                <li>
-                  <Link to="/menu" className="nav-link">Menu </Link>
-                </li>
-                <li>
-                  <Link to="/admin" className="nav-link">Admin </Link>
-                </li>
-                <li>
-                  <Link to="/history" className="nav-link">History </Link>
-                </li>
+  useEffect(() => {
+    handleAutoLogin();
+  }, []);
 
-                {user ? (
-                  <>
-                    <li>
-                    <Link to="/profile" className="nav-link">Profile </Link>
-                    </li>
-                  </>
-                ) : (
-                    <>
-                      <li>
-                        <Link to="/login" className="nav-link">LogIn </Link>
+  return (
+    <nav className="navbar">
+      <div className="navbar-links">
+        <ul>
+          <li>
+            <Link to="/" className="nav-link">
+              {strings.nav.home}
+            </Link>
+          </li>
+          <li>
+            <Link to="/menu" className="nav-link">
+              {strings.nav.menu}
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin" className="nav-link">
+              {strings.nav.admin}
+            </Link>
+          </li>
+          <li>
+            <Link to="/history" className="nav-link">
+              {strings.nav.history}
+            </Link>
+          </li>
 
-                      </li>
-                    </>
-                )}
-                
-                <li>
-                  <Link to="/cart" className="nav-link">Cart </Link>
-                </li>
-            </ul>
-        </div>
+          {user ? (
+            <>
+              <li>
+                <Link to="/profile" className="nav-link">
+                  {strings.nav.profile}
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/login" className="nav-link">
+                  {strings.nav.login}
+                </Link>
+              </li>
+            </>
+          )}
+
+          <li>
+            <Link to="/cart" className="nav-link">
+              {strings.nav.cart}
+            </Link>
+          </li>
+
+          <li>
+            <button onClick={toggleLanguage} className="nav-link">
+              {language === 'fi' ? '🇬🇧 EN' : '🇫🇮 FI'}
+            </button>
+          </li>
+        </ul>
+      </div>
     </nav>
+  );
 }
 
 export default NavBar;
