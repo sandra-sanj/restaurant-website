9764948881e78b36eac305b52c01e4fe76f4f636
nurhana <nur.hana@metropolia.fi>
2025-12-09 09:01:54 +0200

    Add language toggle in Alls.jsx

--- frontend/src/components/menu/All.jsx
+++ frontend/src/components/menu/All.jsx
@@ -1,13 +1,15 @@
 import ShowCards from './ShowCards';
 import HighlightCard from './HighlightCard';
+import {useLanguage} from '../../hooks/useLanguage';
 
 function All() {
-    return(
-        <>
-          <h1>All</h1>
-          <HighlightCard></HighlightCard>
-          <ShowCards ></ShowCards>
-        </>
-    )
-};
+  const {strings} = useLanguage();
+  return (
+    <>
+      <h1>{strings.menu.all}</h1>
+      <HighlightCard></HighlightCard>
+      <ShowCards></ShowCards>
+    </>
+  );
+}
 export default All;
