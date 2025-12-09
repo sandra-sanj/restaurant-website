d75bbda588151881e8dcab72207c936f973779e0
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-12-09 11:48:44 +0200

    change image_url to image_thumb_url in FoodCard.jsx

--- frontend/src/components/menu/FoodCard.jsx
+++ frontend/src/components/menu/FoodCard.jsx
@@ -1,16 +1,13 @@
-
-
 const FoodCard = (props) => {
-  const { item, setSelectedItem} = props;
+  const {item, setSelectedItem} = props;
 
   const API_UPLOADS_URL = import.meta.env.VITE_API_UPLOADS_URL;
 
   return (
     <>
-
       <div className="bg-white-50 w-[500px] rounded-md mb-5 outline-2 outline-gray-400">
         <img
-          src={API_UPLOADS_URL + item.image_url}
+          src={API_UPLOADS_URL + item.image_thumb_url}
           alt={item.description}
           width={'auto'}
           className="rounded-md"
@@ -20,9 +17,7 @@ const FoodCard = (props) => {
           <button>i</button>
         </div>
 
-        <p>
-          {item.description}
-        </p>
+        <p>{item.description}</p>
         <p>{item.price}</p>
         <button onClick={() => setSelectedItem(item)}>Lisää ostoskoriin</button>
       </div>
