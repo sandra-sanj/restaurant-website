ca76c874754f8686f95136e6075fe657705abf76
sandra-sanj <sandra.juursoo@metropolia.fi>
2025-12-09 11:49:35 +0200

    modify image path, add regular price to HighlightCard.jsx

--- frontend/src/components/menu/HighlightCard.jsx
+++ frontend/src/components/menu/HighlightCard.jsx
@@ -1,25 +1,25 @@
 import {useEffect, useState} from 'react';
-import { useMenu } from '../../hooks/apiHook';
+import {useMenu} from '../../hooks/apiHook';
 import AddToCart from '../shoppingcart/AddToCart';
 
+const API_UPLOADS_URL = import.meta.env.VITE_API_UPLOADS_URL;
 
 const HighlightCard = () => {
   const [selectedItem, setSelectedItem] = useState(null);
 
-  const { todaysLunch , error } = useMenu();
-  
+  const {todaysLunch, error} = useMenu();
+
   if (error) return <p>Error: {error}</p>;
 
-  
-  if(todaysLunch) console.log(todaysLunch)
+  if (todaysLunch) console.log('todaysLunch', todaysLunch);
   return (
-  <>
-    {todaysLunch && ( 
-      <div>
-        <AddToCart item={selectedItem} setSelectedItem={setSelectedItem}/>
+    <>
+      {todaysLunch && (
+        <div>
+          <AddToCart item={selectedItem} setSelectedItem={setSelectedItem} />
           <div className="bg-white-50 w-[500px] rounded-md mb-5 outline-10 outline-red-800">
             <img
-              src={todaysLunch.image_url}
+              src={API_UPLOADS_URL + todaysLunch.image_thumb_url}
               alt={todaysLunch.description}
               width={'auto'}
               className="rounded-md"
@@ -27,24 +27,21 @@ const HighlightCard = () => {
             <div>
               <h2>{todaysLunch.name}</h2>
               <p>Päivän lounas</p>
-              <button >i</button>
+              <button>i</button>
             </div>
 
-            <p>
-              {todaysLunch.description}
-            </p>
+            <p>{todaysLunch.description}</p>
             <p>{todaysLunch.special_price}</p>
+            <p className="text-decoration-line: line-through">
+              {todaysLunch.regular_price}
+            </p>
             <button onClick={() => setSelectedItem(todaysLunch)}>
               + Lisää tilaukseen
             </button>
           </div>
-
-
-    </div>
-    )}
-
-      
-  </>
+        </div>
+      )}
+    </>
   );
 };
 
