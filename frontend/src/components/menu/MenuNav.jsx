b94992d596d13549549ae2dee434cab47605953e
nurhana <nur.hana@metropolia.fi>
2025-12-09 09:39:02 +0200

    Add lang in menuNav

--- frontend/src/components/menu/MenuNav.jsx
+++ frontend/src/components/menu/MenuNav.jsx
@@ -1,23 +1,25 @@
-import { Link } from "react-router";
+import {Link} from 'react-router';
+import {useLanguage} from '../../hooks/useLanguage';
 
 const MenuNav = () => {
+  const {strings} = useLanguage();
   return (
     <>
       <ul className="flex flex-row gap-5">
         <li>
-          <Link to="/menu">Kaikki</Link>
+          <Link to="/menu">{strings.menu.all}</Link>
         </li>
         <li>
-          <Link to="/menu/mains">Pääruoat</Link>
+          <Link to="/menu/mains">{strings.menu.mains}</Link>
         </li>
         <li>
-          <Link to="/menu/snacks">Snacks</Link>
+          <Link to="/menu/snacks">{strings.menu.snacks}</Link>
         </li>
         <li>
-          <Link to="/menu/desserts">Desserts</Link>
+          <Link to="/menu/desserts">{strings.menu.desserts}</Link>
         </li>
         <li>
-          <Link to="/menu/drinks">Drinks</Link>
+          <Link to="/menu/drinks">{strings.menu.drinks}</Link>
         </li>
       </ul>
     </>
