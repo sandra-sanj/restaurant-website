c1f60683ac96c0651d160c1fb85608f65712d8f5
Ira <ira.kumpula@metropolia.fi>
2025-12-09 21:08:27 +0200

    Style shopping cart

--- frontend/src/components/shoppingcart/Cart.jsx
+++ frontend/src/components/shoppingcart/Cart.jsx
@@ -1,101 +1,98 @@
-import {useEffect, useState} from "react";
-import { useOrderContext } from "../../hooks/contextHook";
-import OrderCard from "./OrderCard";
-
-
+import {useEffect, useState} from 'react';
+import {useOrderContext} from '../../hooks/contextHook';
+import OrderCard from './OrderCard';
 
 function Cart(props) {
   const {setNext} = props;
-  
-  const [selectedDelivery, setSelectedDelivery] = useState("delivery");
-   const { cart, calculateTotal, setDelivery } = useOrderContext();
+
+  const [selectedDelivery, setSelectedDelivery] = useState('delivery');
+  const {cart, calculateTotal, setDelivery} = useOrderContext();
   const [totalPrice, setTotalPrice] = useState(calculateTotal());
 
-  
-
-
   const handleDeliveryClick = (method) => {
-     setSelectedDelivery(method);
-     setDelivery(method);
-  }
+    setSelectedDelivery(method);
+    setDelivery(method);
+  };
 
- 
-
-  
   console.log(cart);
 
   const totalWithDelivery = Number(totalPrice) + Number(4.9);
 
-
-
-  if (cart.length < 1) return <p>Ostoskorisi on tyhjä</p>
-
-
+  if (cart.length < 1) return <p>Ostoskorisi on tyhjä</p>;
 
   return (
-  <>
-    <div className="m-5 outline-2 outline-gray-400 rounded-md w-[400px]">
-      {/* Header */}
-      <div className="flex justify-center p-4 rounded-t-md">
-        <h1 className="text-lg">Ostoskori</h1>
-      </div>
-      {/* Delivery method*/}
-      <div className="flex flex-col gap-2 p-4 bg-white">
-        <h2 className="font-semibold">Toimitustapa</h2>
-        <div className="flex flex-row justify-center gap-2">
-          
-          <button className={`px-3 py-1 rounded border ${selectedDelivery === "delivery" ? "bg-[#2A4B11]! text-white" : "bg-white text-black"}`} onClick={() => handleDeliveryClick("delivery")}>Toimitus</button>
-
-          <button className={`px-3 py-1 rounded border ${selectedDelivery === "pickup" ? "bg-[#2A4B11]! text-white" : "bg-white text-black"}`} onClick={() => handleDeliveryClick("pickup")}>Nouto</button>
+    <>
+      <div className="m-7 outline-2 outline-stone-500 rounded-md w-[500px]">
+        {/* Header */}
+        <div className="flex justify-center p-4 bg-[#FFFFFF]">
+          <h1 className="text-lg">Ostoskori</h1>
         </div>
-      </div>
-    
+        {/* Delivery method*/}
+        <div className="flex flex-col gap-2 p-4 bg-red-50 border-t border-stone-100">
+          <h2 className="font-semibold text-lg">Toimitustapa</h2>
+          <div className="flex flex-row justify-center gap-2">
+            <button
+              className={`px-3 py-1 rounded border ${selectedDelivery === 'delivery' ? 'bg-[#2A4B11]! text-white' : 'bg-white text-black'}`}
+              onClick={() => handleDeliveryClick('delivery')}
+            >
+              Toimitus
+            </button>
 
-      {/* Products */}
-      <div className="flex flex-col gap-2 p-4 bg-white mt-2">
-        <h2 className="font-semibold">Tuotteet</h2>
-        { cart.map((item) => (
-            <OrderCard 
+            <button
+              className={`px-3 py-1 rounded border ${selectedDelivery === 'pickup' ? 'bg-[#2A4B11]! text-white' : 'bg-white text-black'}`}
+              onClick={() => handleDeliveryClick('pickup')}
+            >
+              Nouto
+            </button>
+          </div>
+        </div>
+
+        {/* Products */}
+        <div className="flex flex-col gap-2 p-4 bg-red-50 border-t border-stone-400">
+          <h2 className="font-semibold text-lg">Tuotteet</h2>
+          {cart.map((item) => (
+            <OrderCard
               key={item.unique_id}
               item={item}
               setTotalPrice={setTotalPrice}
               className="flex justify-between items-center border-b py-2"
-            />)) 
-            }
-            <div className="flex gap-2 items-center">
-              <div>
-                <p className="font-medium"></p>
-                <p>{} </p>
-              </div>
+            />
+          ))}
+          <div className="flex gap-2 items-center">
+            <div>
+              <p className="font-medium"></p>
+              <p>{} </p>
             </div>
           </div>
-        
-      </div>
-      
+        </div>
 
-      {/* Summary */}
-      <div className="flex flex-col gap-1 p-4 bg-white mt-2">
+        {/* Summary */}
+        <div className="flex flex-col gap-1 p-4 bg-red-50 border-t border-stone-400">
           {selectedDelivery === 'delivery' ? (
             <>
               <p>Subtotal {Number(totalPrice).toFixed(2)} €</p>
               <p>Toimitusmaksu 4,90 €</p>
-              <p className="font-semibold">Yhteensä: {totalWithDelivery.toFixed(2)} €</p>
+              <p className="font-semibold">
+                Yhteensä: {totalWithDelivery.toFixed(2)} €
+              </p>
             </>
-
           ) : (
             <p className="font-semibold">Yhteensä: {totalPrice} €</p>
           )}
-          
-      </div>
+        </div>
 
-      {/* Checkout button */}
-      <div className="p-4 bg-white mt-2 flex">
-        <button onClick={() => setNext('order')} className="w-full py-2 rounded border bg-[#2A4B11]! text-white">Kassalle</button>
+        {/* Checkout button */}
+        <div className="p-5 bg-white flex border-t border-stone-100">
+          <button
+            onClick={() => setNext('order')}
+            className="w-full py-2 rounded border bg-[#2A4B11]! text-white"
+          >
+            Kassalle
+          </button>
+        </div>
       </div>
- 
-</>
-
-);
+    </>
+  );
 }
 
 export default Cart;
--- frontend/src/components/shoppingcart/OrderCard.jsx
+++ frontend/src/components/shoppingcart/OrderCard.jsx
@@ -1,42 +1,54 @@
-import { useEffect, useState} from "react";
-import { useOrderContext } from "../../hooks/contextHook";
+import {useEffect, useState} from 'react';
+import {useOrderContext} from '../../hooks/contextHook';
 
 //this is a card for menu item in shopping cart
 
 const OrderCard = (props) => {
-    const {handleRemoveItem, calculateTotal} = useOrderContext();
-    const {item, setTotalPrice} = props;
-    const [quantity, setQuantity] = useState(item.quantity);
-    
+  const {handleRemoveItem, calculateTotal} = useOrderContext();
+  const {item, setTotalPrice} = props;
+  const [quantity, setQuantity] = useState(item.quantity);
 
   useEffect(() => {
     item.quantity = quantity;
     setTotalPrice(calculateTotal());
-    if(quantity == 0) {
+    if (quantity == 0) {
       handleRemoveItem(item.unique_id);
       setTotalPrice(calculateTotal());
     }
   }, [quantity]);
 
+  return (
+    <div className="mb-2 flex flex-row gap-5">
+      <div className='flex flex-col min-w-[200px] text-left justify-center'>
+        <p className="pl-5">{item.item_name}</p>
+        <p className='pl-5'>{(quantity * item.unit_price).toFixed(2)} €</p>
+      </div>
 
-    return (
-        <div>
-          <p>{item.item_name}</p>
-          <p>{(quantity * item.unit_price).toFixed(2)} €</p>
+      <div className="flex flex-row w-full gap-2 items-center justify-end">
+        <button
+          onClick={() => setQuantity(0)}
+          className="rounded-3xl! bg-[#982A2A]! hover:bg-[#982A2a80]! border! border-stone-500! py-1.5! px-3.5! text-white"
+        >
+          x
+        </button>
 
-
-          <div className="flex gap-2 items-center">
-              <button onClick={() => setQuantity(0)} className="border rounded bg-[#982A2A]! text-white">x</button>
-
-
-              <div className="flex items-center gap-1">
-                <button onClick={() => setQuantity(q => Math.max(0, q - 1))} className="px-2 py-1 border rounded">-</button>
-                <span>{quantity}</span>
-                <button onClick={() => setQuantity(q => q + 1)} className="px-2 py-1 border rounded">+</button>
-              </div>
-            </div> 
+        <div className="flex items-center gap-1">
+          <button
+            onClick={() => setQuantity((q) => Math.max(0, q - 1))}
+            className="px-3! py-1! border! border-stone-500! rounded-lg! hover:bg-stone-200!"
+          >
+            -
+          </button>
+          <span>{quantity}</span>
+          <button
+            onClick={() => setQuantity((q) => q + 1)}
+            className="px-2.5! py-1! border! border-stone-500! rounded-lg! hover:bg-stone-200!"
+          >
+            +
+          </button>
         </div>
-    )
-    
+      </div>
+    </div>
+  );
 };
 export default OrderCard;
