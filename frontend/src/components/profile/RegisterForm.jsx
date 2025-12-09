d79c20a7191be552645c475b912508514c2dbf4f
Ira <ira.kumpula@metropolia.fi>
2025-12-09 20:35:04 +0200

    Style register form

--- frontend/src/components/profile/RegisterForm.jsx
+++ frontend/src/components/profile/RegisterForm.jsx
@@ -2,74 +2,86 @@ import useForm from '../../hooks/formHooks';
 import { useUser } from '../../hooks/apiHook';
 
 const RegisterForm = () => {
+  const { postUser } = useUser();
 
-    const initValues = {
+  const initValues = {
     username: '',
     password: '',
     email: '',
     phone: '',
-    };
+  };
 
-    const doRegister = () => {
-        try {
-            inputs.role = 'customer';
-            inputs.is_active = 1;
-            const userInfo = postUser(inputs);
+  const doRegister = () => {
+    try {
+      inputs.role = 'customer';
+      inputs.is_active = 1;
+      const userInfo = postUser(inputs);
+    } catch (error) {
+      console.log(error);
+    }
+    return;
+  };
 
-        } catch (error) {
-            console.log(error);
-        }
-        return;
-    };
+  const { inputs, handleInputChange, handleSubmit } = useForm(doRegister, initValues);
 
-    const { postUser } = useUser();
-
-    const {inputs, handleInputChange, handleSubmit} = useForm(doRegister, initValues);
-
-     return (
-         <>
-            <h1> Luo käyttäjä</h1>
-            <form onSubmit={ (e) => {handleSubmit(e)} }>
-                <div>
-                    <label htmlFor="registerUser">Käyttäjänimi: </label>
-                    <input
-                        name="username"
-                        type="text"
-                        id="registerUser"
-                        onChange={ (e) => {handleInputChange(e)} }
-                    />
-                </div>
-                <div>
-                    <label htmlFor="registerEmail">Sähköposti: </label>
-                    <input
-                        name="email"
-                        type="text"
-                        id="registerEmail"
-                        onChange={ (e) => {handleInputChange(e)} }
-                    />
-                </div>
-                <div>
-                    <label htmlFor="registerPassword">Salasana: </label>
-                    <input
-                        name="password"
-                        type="password"
-                        id="registerPassword"
-                        onChange={ (e) => {handleInputChange(e)} }
-                    />
-                </div>
-                <div>
-                    <label htmlFor="registerPhone">Puhelinnumero (muoto +358): </label>
-                    <input
-                        name="phone"
-                        type="text"
-                        id="registerPhone"
-                        onChange={ (e) => {handleInputChange(e)} }
-                    />
-                </div>
-                <button type="submit">Luo käyttäjä</button>
-            </form>
-        </>
-    );
+  return (
+    <div>
+      <h1 className='mb-8 mt-5'>Rekisteröidy</h1>
+      <div>
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <div className='bg-[#FFFFFF] px-10 py-15 flex flex-col gap-8 w-[500px] border border-stone-300 rounded-md'>
+            <div className='flex flex-row items-start w-full'>
+              <label htmlFor="registerUser" className="text-left mr-12">Käyttäjänimi: </label>
+              <input
+                name="username"
+                type="text"
+                id="registerUser"
+                onChange={handleInputChange}
+                className='w-full'
+              />
+            </div>
+            <div className='flex flex-row items-start w-full'>
+              <label htmlFor="registerEmail" className="text-left mr-14.5">Sähköposti: </label>
+              <input
+                name="email"
+                type="text"
+                id="registerEmail"
+                onChange={handleInputChange}
+                className='w-full'
+              />
+            </div>
+            <div className='flex flex-row items-start w-full'>
+              <label htmlFor="registerPassword" className="text-left mr-19">Salasana: </label>
+              <input
+                name="password"
+                type="password"
+                id="registerPassword"
+                onChange={handleInputChange}
+                className='w-full'
+              />
+            </div>
+            <div className='flex flex-row items-start w-full'>
+              <label htmlFor="registerPhone" className="text-left mr-0">Puhelinnumero (muoto +358): </label>
+              <input
+                name="phone"
+                type="text"
+                id="registerPhone"
+                onChange={handleInputChange}
+                className='w-full'
+              />
+            </div>
+          </div>
+          <button type="submit" className='mt-4 bg-[#982A2A]! text-white hover:bg-[#982A2a90]! py-2 rounded'>
+            Luo käyttäjä
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterForm;
