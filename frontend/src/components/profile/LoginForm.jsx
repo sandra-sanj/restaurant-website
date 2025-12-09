cee6048b4c5a3877f1993789c26894f5c055013c
nurhana <nur.hana@metropolia.fi>
2025-12-09 16:01:50 +0200

    Add lang switch in LoginForm

--- frontend/src/components/profile/LoginForm.jsx
+++ frontend/src/components/profile/LoginForm.jsx
@@ -1,54 +1,65 @@
-import { useUserContext } from "../../hooks/contextHook";
-import useForm from "../../hooks/formHooks";
+import {useUserContext} from '../../hooks/contextHook';
+import {useLanguage} from '../../hooks/useLanguage';
+import useForm from '../../hooks/formHooks';
 
 const LoginForm = () => {
-    const { handleLogin } = useUserContext();
+  const {handleLogin} = useUserContext();
+  const {strings} = useLanguage();
 
-    const initValues = {
+  const initValues = {
     username: '',
     password: '',
-    };
+  };
 
+  const doLogin = async () => {
+    try {
+      handleLogin(inputs);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const doLogin = async () => {
-        try {
-            handleLogin(inputs);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const {inputs, handleInputChange, handleSubmit} = useForm(
+    doLogin,
+    initValues,
+  );
 
-
-    const {inputs, handleInputChange, handleSubmit} = useForm(doLogin, initValues);
-
-     return (
-         <>
-             <h1>Kirjaudu sisään!</h1>
-             <form onSubmit={ (e) => {handleSubmit(e)} }>
-                  <div>
-                      <label htmlFor="loginuser">Käyttäjänimi: </label>
-                     <input
-                         name="username"
-                         type="text"
-                         id="loginuser"
-                         onChange={ (e) => {handleInputChange(e)} }
-                         autoComplete="username"
-                     />
-                 </div>
-                 <div>
-                     <label htmlFor="loginpassword">Salasana: </label>
-                      <input
-                         name="password"
-                         type="password"
-                         id="loginpassword"
-                         onChange={ (e) => {handleInputChange(e)} }
-                         autoComplete="current-password"
-                     />
-                 </div>
-                 <button type="submit">Kirjaudu</button>
-             </form>
-         </>
-     );
+  return (
+    <>
+      <h1>{strings.auth.loginTitle}</h1>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <div>
+          <label htmlFor="loginuser">{strings.auth.username}: </label>
+          <input
+            name="username"
+            type="text"
+            id="loginuser"
+            onChange={(e) => {
+              handleInputChange(e);
+            }}
+            autoComplete="username"
+          />
+        </div>
+        <div>
+          <label htmlFor="loginpassword">{strings.auth.password}: </label>
+          <input
+            name="password"
+            type="password"
+            id="loginpassword"
+            onChange={(e) => {
+              handleInputChange(e);
+            }}
+            autoComplete="current-password"
+          />
+        </div>
+        <button type="submit">{strings.auth.loginButton}</button>
+      </form>
+    </>
+  );
 };
 
 export default LoginForm;
