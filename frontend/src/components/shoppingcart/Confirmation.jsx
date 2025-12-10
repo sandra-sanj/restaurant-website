68d07ee74097b4f5122b6f0ca4ca4d026dd793c8
Anni <anni.alanen@metropolia.fi>
2025-12-10 22:12:32 +0200

    confirmation done

--- frontend/src/components/shoppingcart/Confirmation.jsx
+++ frontend/src/components/shoppingcart/Confirmation.jsx
@@ -10,7 +10,6 @@ const Confirmation = () => {
         <div className="m-7 outline-2 outline-stone-500 rounded-md w-[500px]">
           <div>
             <h1 className="text-lg">Kiitos tilauksesta! </h1>
-            <p>Jos tilauksen kanssa on ongelmia, olethan yhteydessä ravintolaan (+358 7776669)</p>
             <div>
               <p>Order ID:{orderResult.order_id}</p>
             <p>{orderResult.order_type}</p>
@@ -36,13 +35,12 @@ const Confirmation = () => {
             <p></p>
             </div>
             <div className="flex flex-col gap-1 p-4 bg-red-50 border-t border-stone-400">
-          {/*selectedDelivery === 'delivery' ?*/ (
               <p>{orderResult.total_price} €</p>
               
               
-          )  (
+          
             <p className="font-semibold">Yhteensä:  €</p>
-          )}
+          
         </div>
           </div>
 
