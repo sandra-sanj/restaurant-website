af1966b704716cb17135af9429c8b767bcf4df04
Anni <anni.alanen@metropolia.fi>
2025-12-10 18:00:38 +0200

    accidently hided the wrong thing last time

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,22 +13,34 @@ function Home() {
   const imgSrc = '/images/';
 
   return (
+
+
+        
     <div className="flex flex-col self-auto">
       <div className="flex flex-col justify-center items-center w-full">
         <h1 className="p-5 lg:mt-5">Taqueria 21</h1>
         <p className="mb-5 text-lg">{strings.home.tagline}</p>
       </div>
-
-      
-
-      
+       {!user && ( 
+        <div className="flex flex-row items-center w-full justify-center">
+          <button
+            onClick={() => navigate('/login')}
+            className="bg-[#982A2A]! text-white hover:bg-[#792121]!"
+          >
+            {strings.nav.login}
+          </button>
+          <button
+            onClick={() => navigate('/login/register')}
+            className="bg-[#982A2A]! text-white hover:bg-[#792121]!"
+          >
+            {strings.home.register}
+          </button>
+        </div>
+        )};
 
       <div className="ad-div w-screen flex items-center justify-center gap-2 bg-[#982A2A] lg:mt-8">
         
         <div className="flex flex-row p-5 gap-4 lg:gap-15 lg:p-15 lg:text-lg">
-
-
-        {!user && (  
           <div>
             <img
               src={imgSrc + '/home/home2.jpg'}
@@ -41,7 +53,7 @@ function Home() {
             <p className="text-white mt-2">{strings.home.newItemLabel}:</p>
             <p className="text-white">{strings.home.newItemName}</p>
           </div>
-        )};
+
           <div>
             <p className="text-white">{strings.home.chefFavoriteLabel}:</p>
             <p className="text-white mb-2">{strings.home.chefFavoriteName}</p>
