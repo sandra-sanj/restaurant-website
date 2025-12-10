b2e28adf6382f5d4347e21a1402bce6f43d5851f
nurhana <nur.hana@metropolia.fi>
2025-12-10 09:26:16 +0200

    Not modified, autoformat affected the file

--- frontend/src/components/profile/LoginForm.jsx
+++ frontend/src/components/profile/LoginForm.jsx
@@ -26,16 +26,18 @@ const LoginForm = () => {
 
   return (
     <div>
-      <h1 className='mb-8'>{strings.auth.loginTitle}</h1>
+      <h1 className="mb-8">{strings.auth.loginTitle}</h1>
       <div className="">
         <form
           onSubmit={(e) => {
             handleSubmit(e);
           }}
         >
-          <div className='bg-[#FFFFFF] px-10 py-15 flex flex-col gap-8 w-[400px] border border-stone-300! rounded-md'>
-            <div className='flex flex-row items-start w-full'>
-              <label htmlFor="loginuser" className="text-left mr-4">{strings.auth.username}: </label>
+          <div className="bg-[#FFFFFF] px-10 py-15 flex flex-col gap-8 w-[400px] border border-stone-300! rounded-md">
+            <div className="flex flex-row items-start w-full">
+              <label htmlFor="loginuser" className="text-left mr-4">
+                {strings.auth.username}:{' '}
+              </label>
               <input
                 name="username"
                 type="text"
@@ -44,11 +46,13 @@ const LoginForm = () => {
                   handleInputChange(e);
                 }}
                 autoComplete="username"
-                className='w-full'
+                className="w-full"
               />
             </div>
-            <div className='flex flex-row items-start w-full'>
-              <label htmlFor="loginpassword" className="text-left mr-10">{strings.auth.password}: </label>
+            <div className="flex flex-row items-start w-full">
+              <label htmlFor="loginpassword" className="text-left mr-10">
+                {strings.auth.password}:{' '}
+              </label>
               <input
                 name="password"
                 type="password"
@@ -57,11 +61,16 @@ const LoginForm = () => {
                   handleInputChange(e);
                 }}
                 autoComplete="current-password"
-                className='w-full'
+                className="w-full"
               />
             </div>
           </div>
-          <button type="submit" className='bg-[#982A2A]! text-white hover:bg-[#982A2a90]!'>{strings.auth.loginButton}</button>
+          <button
+            type="submit"
+            className="bg-[#982A2A]! text-white hover:bg-[#982A2a90]!"
+          >
+            {strings.auth.loginButton}
+          </button>
         </form>
       </div>
     </div>
