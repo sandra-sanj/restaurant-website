161aa03a8eaaadc8ea70a84dc59f85dfa6301785
nurhana <nur.hana@metropolia.fi>
2025-12-11 00:31:29 +0200

    Add lang string OrderedItem.jsx

--- frontend/src/components/shoppingcart/OrderedItem.jsx
+++ frontend/src/components/shoppingcart/OrderedItem.jsx
@@ -1,27 +1,27 @@
+import {useLanguage} from '../../hooks/useLanguage';
 
 const OrderedItem = (props) => {
-    const {item} = props;
+  const {item} = props;
+  const {strings} = useLanguage();
 
-    return (
+  return (
     <>
-      {item &&(
+      {item && (
         <div className="mb-2 flex flex-row gap-5">
-        <div className='flex flex-col min-w-[200px] text-left justify-center'>
-        <p className="pl-5">{item.item_name}</p>
-        <p className="pl-5">{item.quantity} kpl</p>
+          <div className="flex flex-col min-w-[200px] text-left justify-center">
+            <p className="pl-5">{item.item_name}</p>
+            <p className="pl-5">
+              {item.quantity} {strings.cart?.pieces || 'kpl'}
+            </p>
+          </div>
+          <div className="flex flex-row w-full gap-2 items-center justify-end">
+            <div className="flex items-center gap-1">
+              <span>{item.unit_price} €</span>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-row w-full gap-2 items-center justify-end">
-
-        <div className="flex items-center gap-1">
-          
-          <span>{item.unit_price} €</span>
-          
-        </div>
-      </div>
-      </div>
-    )}
-    
-    </> 
-
-)};
+      )}
+    </>
+  );
+};
 export default OrderedItem;
