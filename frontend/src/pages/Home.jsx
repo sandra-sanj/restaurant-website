17642b2cec74c4d3e160377215996e91674dd469
Ira <ira.kumpula@metropolia.fi>
2025-12-10 12:02:37 +0200

    Add large screen layout to Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
 
   return (
     <div className="flex flex-col self-auto">
-      <div className="flex flex-col justify-center items-center w-[100%]">
+      <div className="flex flex-col justify-center items-center w-full">
         <h1 className="p-5">Taqueria 21</h1>
         <p className="mb-5 text-lg">{strings.home.tagline}</p>
       </div>
@@ -34,7 +34,7 @@ function Home() {
 
       <div className="ad-div w-screen flex items-center justify-center gap-2 bg-[#982A2A]">
         
-        <div className="flex flex-row p-5 gap-4">
+        <div className="flex flex-row p-5 gap-4 lg:p-10 lg:text-lg">
           <div>
             <img
               src={imgSrc + '/home/home2.jpg'}
@@ -42,7 +42,7 @@ function Home() {
               /*width="200"
               height="500"*/
               onClick={() => navigate('/menu')}
-              className="overflow-hidden h-[265px] max-w-[200px] rounded-sm cursor-pointer outline-1 outline-zinc-300 hover:brightness-85"
+              className="overflow-hidden h-[265px] lg:h-[329px] max-w-[200px] lg:max-w-[250px] rounded-sm cursor-pointer outline-1 outline-zinc-300 hover:brightness-85"
             />
             <p className="text-white mt-2">{strings.home.newItemLabel}:</p>
             <p className="text-white">{strings.home.newItemName}</p>
@@ -57,7 +57,7 @@ function Home() {
               /*width="200"
               height="500"*/
               onClick={() => navigate('/menu')}
-              className="overflow-hidden h-[265px] max-w-[200px] rounded-sm cursor-pointer outline-1 outline-zinc-300 hover:brightness-85"
+              className="overflow-hidden h-[265px] lg:h-[329px] max-w-[200px] lg:max-w-[250px] rounded-sm cursor-pointer outline-1 outline-zinc-300 hover:brightness-85"
             />
           </div>
         </div>
@@ -65,18 +65,18 @@ function Home() {
 
       <div className="mt-6 flex flex-col items-center justify-center">
         <h3
-          className="mb-6 text-xl font-bold cursor-pointer"
+          className="mb-6 text-xl lg:text-2xl font-bold cursor-pointer"
           onClick={() => navigate('/menu')}
         >
           {strings.home.menuTitle}
         </h3>
-        <div className="container-home mb-15 grid grid-cols-2 md:grid-cols-4 gap-4 content-center justify-items-center max-w-[1000px]">
+        <div className="container-home mb-15 grid grid-cols-2 md:grid-cols-4 gap-6 content-center justify-items-center max-w-[1000px] lg:gap-15 lg:text-lg">
           <div className="flex flex-col justify-center items-center">
             <img
               src={imgSrc + '/home/mains.jpg'}
               alt="Tacos"
               onClick={() => navigate('/menu/mains')}
-              className="rounded-sm cursor-pointer outline-1 outline-zinc-300 hover:brightness-85"
+              className="rounded-sm cursor-pointer outline-1 outline-zinc-300 hover:brightness-85 h-40 w-40 object-cover lg:h-48 lg:w-48"
             />
             <p className="mt-2 text-md">{strings.menu.mains.toUpperCase()}</p>
           </div>
@@ -85,7 +85,7 @@ function Home() {
               src={imgSrc + '/home/snacks.jpg'}
               alt="Corn chips"
               onClick={() => navigate('/menu/snacks')}
-              className="rounded-sm cursor-pointer outline-1 outline-zinc-300 hover:brightness-85"
+              className="rounded-sm cursor-pointer outline-1 outline-zinc-300 hover:brightness-85 h-40 w-40 object-cover lg:h-48 lg:w-48"
             />
             <p className="mt-2 text-md">{strings.menu.snacks.toUpperCase()}</p>
           </div>
@@ -94,7 +94,7 @@ function Home() {
               src={imgSrc + '/home/desserts.jpg'}
               alt="Mexican caramel pudding"
               onClick={() => navigate('/menu/desserts')}
-              className="rounded-sm cursor-pointer outline-1 outline-zinc-300 hover:brightness-85"
+              className="rounded-sm cursor-pointer outline-1 outline-zinc-300 hover:brightness-85 h-40 w-40 object-cover lg:h-48 lg:w-48"
             />
             <p className="mt-2 text-md">
               {strings.menu.desserts.toUpperCase()}
@@ -105,7 +105,7 @@ function Home() {
               src={imgSrc + '/home/drinks.jpg'}
               alt="Jarritos bottles"
               onClick={() => navigate('/menu/drinks')}
-              className="rounded-sm cursor-pointer outline-1 outline-zinc-300 hover:brightness-85"
+              className="rounded-sm cursor-pointer outline-1 outline-zinc-300 hover:brightness-85 h-40 w-40 object-cover lg:h-48 lg:w-48"
             />
             <p className="mt-2 text-md">{strings.menu.drinks.toUpperCase()}</p>
           </div>
