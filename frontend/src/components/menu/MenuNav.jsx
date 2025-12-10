61873d4e7b46c53f0af6eb083f030e2ec0ffef30
Ira <ira.kumpula@metropolia.fi>
2025-12-11 00:16:26 +0200

    Style MenuNav

--- frontend/src/components/menu/MenuNav.jsx
+++ frontend/src/components/menu/MenuNav.jsx
@@ -1,28 +1,57 @@
 import {Link} from 'react-router';
 import {useLanguage} from '../../hooks/useLanguage';
+import { NavLink } from 'react-router-dom';
+import { useLocation } from 'react-router';
 
 const MenuNav = () => {
   const {strings} = useLanguage();
+  const location = useLocation();
+
   return (
-    <>
-      <ul className="flex flex-row gap-5">
-        <li>
-          <Link to="/menu">{strings.menu.all}</Link>
-        </li>
-        <li>
-          <Link to="/menu/mains">{strings.menu.mains}</Link>
-        </li>
-        <li>
-          <Link to="/menu/snacks">{strings.menu.snacks}</Link>
-        </li>
-        <li>
-          <Link to="/menu/desserts">{strings.menu.desserts}</Link>
-        </li>
-        <li>
-          <Link to="/menu/drinks">{strings.menu.drinks}</Link>
-        </li>
+    <div>
+      <ul className="flex flex-col sm:flex-row gap-0 sm:gap-5 p-0! bg-[#982A2A] text-white rounded-4xl">
+        <div className="p-2">
+          <li className='rounded-3xl p-0!'>
+            <NavLink to="/menu"
+            className={() => location.pathname.endsWith('/menu') ? "border-white border rounded-3xl" : "border-[#982A2A] border rounded-3xl hover:border-white"}
+            >{strings.menu.all}</NavLink>
+          </li>
+        </div>
+
+        <div className="p-2">
+          <li className='rounded-3xl p-0!'>
+            <NavLink to="/menu/mains"
+            className={() => location.pathname.endsWith('/menu/mains') ? "border-white border rounded-3xl" : "border-[#982A2A] border rounded-3xl hover:border-white"}
+            >{strings.menu.mains}</NavLink>
+          </li>
+        </div>
+
+        <div className="p-2">
+          <li className='rounded-3xl p-0!'>
+            <NavLink to="/menu/snacks"
+            className={() => location.pathname.endsWith('/menu/snacks') ? "border-white border rounded-3xl" : "border-[#982A2A] border rounded-3xl hover:border-white"}
+            >{strings.menu.snacks}</NavLink>
+          </li>
+        </div>
+
+        <div className="p-2">
+          <li className='rounded-3xl p-0!'>
+            <NavLink to="/menu/desserts"
+            className={() => location.pathname.endsWith('/menu/desserts') ? "border-white border rounded-3xl" : "border-[#982A2A] border rounded-3xl hover:border-white"}
+            >{strings.menu.desserts}</NavLink>
+          </li>
+        </div>
+
+       <div className="p-2">
+          <li className='rounded-3xl p-0!'>
+            <NavLink to="/menu/drinks"
+            className={() => location.pathname.endsWith('/menu/drinks') ? "border-white border rounded-3xl" : "border-[#982A2A] border rounded-3xl hover:border-white"}
+            >{strings.menu.drinks}</NavLink>
+          </li>
+        </div>
+
       </ul>
-    </>
+    </div>
   );
 };
 
