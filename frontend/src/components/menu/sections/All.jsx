c01272ec631cb8318a3b3d3fea61d8b7f6eb3e60
Ira <ira.kumpula@metropolia.fi>
2025-12-11 13:17:09 +0200

    Remove duplicate headings from menu sections (menu nav bar has headings)

--- frontend/src/components/menu/sections/All.jsx
+++ frontend/src/components/menu/sections/All.jsx
@@ -1,12 +1,9 @@
 import ShowCards from '../ShowCards';
 import HighlightCard from '../HighlightCard';
-import {useLanguage} from '../../../hooks/useLanguage';
 
 function All() {
-  const {strings} = useLanguage();
   return (
     <>
-      <h1>{strings.menu.all}</h1>
       <HighlightCard></HighlightCard>
       <ShowCards></ShowCards>
     </>
