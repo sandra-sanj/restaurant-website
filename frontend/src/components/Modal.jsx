9057c5f9cd6be2eee6a93b413fddf026526f6f83
Ira <ira.kumpula@metropolia.fi>
2025-12-11 11:56:58 +0200

    Wrap admin's AddItem inside Modal - add "unstyled" prop to Modal to keep AddItem styling

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-function Modal({isOpen, onClose, children}) {
+function Modal({isOpen, onClose, children, unstyled=false}) {
   if (!isOpen) return null; //wont show modal if isOpen is false
 
   return (
     <div
-      className="fixed inset-0 bg-black/20 flex items-center justify-center z-50"
+      className="fixed inset-0 bg-black/20 flex items-center justify-center z-50 overflow-y-scroll"
       onClick={onClose}
     >
+
+      {/* Unstyled for larger modals in admin page */}
+      {unstyled ? (
+        <div onClick={(e) => e.stopPropagation()}
+        className='mt-10'>
+          {children}
+        </div>
+      ) : (
+
       <div
-        className="m-6 outline-2 outline-gray-400 rounded-md  w-[300px] xs:w-[350px] bg-[#FFFFFF] pb-3"
+        className="m-6 outline-2 outline-gray-400 rounded-md w-[300px] xs:w-[350px] sm:w-[400px] bg-[#FFFFFF] pb-3"
         onClick={onClose}
       >
         <span
@@ -25,6 +34,7 @@ function Modal({isOpen, onClose, children}) {
           {children}
         </div>
       </div>
+      )}
     </div>
   );
 }
